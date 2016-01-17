f1f70ddeca352f2d2addab4cccac88a4f000bfec
Christos Bacharakis <christos@bacharakis.com>
2016-01-17 04:07:50 +0200

    Add working form

--- src/js/creative.js
+++ src/js/creative.js
@@ -79,3 +79,20 @@ $.get('hackers.txt',function (data){
       }
       document.getElementById("status").textContent=text;
     });
+
+/** This section is only needed once per page if manually copying **/
+if (typeof MauticSDKLoaded == 'undefined') {
+    var MauticSDKLoaded = true;
+    var head            = document.getElementsByTagName('head')[0];
+    var script          = document.createElement('script');
+    script.type         = 'text/javascript';
+    script.src          = 'https://techministry.mautic.com/mautic/media/js/mautic-form.js';
+    script.onload       = function() {
+        MauticSDK.onLoad();
+    };
+    head.appendChild(script);
+    var MauticDomain = 'https://techministry.mautic.com';
+    var MauticLang   = {
+        'submittingMessage': "Please wait..."
+    }
+}
